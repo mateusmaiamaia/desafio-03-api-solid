632ce99f9a5b302f0d6014d6189b3e326344f55e
mateusmaiamaia <mateusmaiamaia@hotmail.com>
2025-07-24 12:15:54 -0300

    fix

--- packages/web/src/components/DefaultLayout/index.tsx
+++ packages/web/src/components/DefaultLayout/index.tsx
@@ -1,14 +1,40 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useOutletContext } from 'react-router-dom'
 import { Sidebar } from '../Sidebar'
 import styles from './styles.module.css'
+import { useState } from 'react'
+import { api } from '@/services/api'
+
+interface Pet {
+  id: string
+  name: string
+}
+
+type OutletContextType = {
+  pets: Pet[]
+}
+
+export function usePets() {
+  return useOutletContext<OutletContextType>()
+}
 
 export function DefaultLayout() {
+  const [pets, setPets] = useState<Pet[]>([])
+
+  async function searchPets(city: string) {
+    try {
+      const response = await api.get('/pets', {
+        params: { city },
+      })
+      setPets(response.data.pets)
+    } catch (error) {
+      console.error('Erro ao buscar pets:', error)
+    }
+  }
+
   return (
     <div className={styles.layout}>
-      <Sidebar onSearch={function (city: string): void {
-        throw new Error('Function not implemented.')
-      } } />
-      <Outlet />
+      <Sidebar onSearch={searchPets} />
+      <Outlet context={{ pets }} />
     </div>
   )
 }
